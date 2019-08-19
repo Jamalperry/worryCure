293d8eeaf04717cba029821f81fd5d4d7d409034
Jamalperry <lee.perry400@gmail.com>
2019-08-19 15:57:44 -0400

    add initial build to github

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+function myThings1() {
+  document.getElementById("med1").innerHTML = "All things that is in harmony with eternaltruth in creationand revelation.";
+}
+	
+function myVerse1() {
+	document.getElementById("verse1").innerHTML = "Study and do your best to present yourself to God approved, a workman [tested by trial] who has no reason to be ashamed, accurately handling and skillfully teaching the word of truth... All Scripture is God-breathed [given by divine inspiration] and is profitable for instruction, for conviction [of sin], for correction [of error and restoration to obedience], for training in righteousness [learning to live in conformity to God’s will, both publicly and privately--behaving honorably with personal integrity and moral courage]; so that the man of God may be complete and proficient, outfitted and thoroughly equipped for every good work.";
+}
+
+function myThings2() {
+	document.getElementById("med2").innerHTML = "All that is grave, decent, honorable, and Christian Gr. semnos.";
+}
+	
+function myVerse2() {
+	document.getElementById("verse2").innerHTML = "#";
+}
+
+function myThings3() {
+	document.getElementById("med3").innerHTML = "#";
+}
+	
+function myVerse3() {
+	document.getElementById("verse3").innerHTML = "#";
+}
+
+function myThings4() {
+	document.getElementById("med4").innerHTML = "#";
+}
+	
+function myVerse4() {
+	document.getElementById("verse4").innerHTML = "#";
+}
+
+function myThings5() {
+	document.getElementById("med5").innerHTML = "#";
+}
+	
+function myVerse5() {
+	document.getElementById("verse5").innerHTML = "#";
+}
+
+function myThings6() {
+	document.getElementById("med6").innerHTML = "#";
+}
+	
+function myVerse6() {
+	document.getElementById("verse6").innerHTML = "#";
+}
+
+
+function myFunction() {
+  document.getElementById("8live").innerHTML = "For sin will no longer be a master over you, since you are not under Law [as slaves], but under [unmerited] grace [as recipients of God’s favor and mercy]. What then [are we to conclude]? Shall we sin because we are not under Law, but under [God’s] grace? Certainly not! Do you not know that when you continually offer yourselves to someone to do his will, you are the slaves of the one whom you obey, either [slaves] of sin, which leads to death, or of obedience, which leads to righteousness (right standing with God)? But thank God that though you were slaves of sin, you became obedient with all your heart to the standard of teaching in which you were instructed and to which you were committed. And having been set free from sin, you have become the slaves of righteousness [of conformity to God’s will and purpose]. I am speaking in [familiar] human terms because of your natural limitations [your spiritual immaturity]. For just as you presented your bodily members as slaves to impurity and to [moral] lawlessness, leading to further lawlessness, so now offer your members [your abilities, your talents] as slaves to righteousness, leading to sanctification [that is, being set apart for God’s purpose]. When you were slaves of sin, you were free in regard to righteousness [you had no desire to conform to God’s will]. So what benefit did you get at that time from the things of which you are now ashamed? [None!] For the outcome of those things is death! But now since you have been set free from sin and have become [willing] slaves to God, you have your benefit, resulting in sanctification [being made holy and set apart for God’s purpose], and the outcome [of this] is eternal life. For the wages of sin is death, but the free gift of God [that is, His remarkable, overwhelming gift of grace to believers] is eternal life in Christ Jesus our Lord.";
+}
+
+function myFunction2() {
+	document.getElementById("8live2").innerHTML = "Therefore there is now no condemnation [no guilty verdict, no punishment] for those who are in Christ Jesus [who believe in Him as personal Lord and Savior]. For the law of the Spirit of life [which is] in Christ Jesus [the law of our new being] has set you free from the law of sin and of death. For what the Law could not do [that is, overcome sin and remove its penalty, its power] being weakened by the flesh [man’s nature without the Holy Spirit], God did: He sent His own Son in the likeness of sinful man as an offering for sin.";
+}
+
+function myFunction3() {
+	document.getElementById("8live3").innerHTML = "It does not submit itself to God’s law, since it cannot, and those who are in the flesh [living a life that caters to sinful appetites and impulses] cannot please God. However, you are not [living] in the flesh [controlled by the sinful nature] but in the Spirit, if in fact the Spirit of God lives in you [directing and guiding you]. But if anyone does not have the Spirit of Christ, he does not belong to Him [and is not a child of God]. If Christ lives in you, though your [natural] body is dead because of sin, your spirit is alive because of righteousness [which He provides]. And if the Spirit of Him who raised Jesus from the dead lives in you, He who raised Christ Jesus from the dead will also give life to your mortal bodies through His Spirit, who lives in you. So then, brothers and sisters, we have an obligation, but not to our flesh [our human nature, our worldliness, our sinful capacity], to live according to the [impulses of the] flesh [our nature without the Holy Spirit]-- for if you are living according to the [impulses of the] flesh, you are going to die.";
+}
+
+function myFunction4() {
+	document.getElementById("8live4").innerHTML = "For you have not received a spirit of slavery leading again to fear [of God’s judgment], but you have received the Spirit of adoption as sons [the Spirit producing sonship] by which we [joyfully] cry, “ Abba! Father!” The Spirit Himself testifies and confirms together with our spirit [assuring us] that we [believers] are children of God. And if [we are His] children, [then we are His] heirs also: heirs of God and fellow heirs with Christ [sharing His spiritual blessing and inheritance], if indeed we share in His suffering so that we may also share in His glory.";
+}
+
